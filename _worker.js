d2e8418e85db0d55e6fbc878cb1ffb1c16d94059
Hugh <haoyg@users.noreply.github.com>
2026-05-26 09:37:46 +0800

    bug fixed

--- _worker.js
+++ _worker.js
@@ -9,6 +9,8 @@ const SPA_ROUTES = new Set([
   "/idle-games"
 ]);
 
+const ADS_TXT = "google.com, pub-3274781156049995, DIRECT, f08c47fec0942fa0\n";
+
 function normalizePath(pathname) {
   return pathname.replace(/\/+$/, "") || "/";
 }
@@ -20,10 +22,19 @@ function shouldServeAppShell(pathname) {
 
 export default {
   async fetch(request, env) {
+    const url = new URL(request.url);
+    if (url.pathname === "/ads.txt") {
+      return new Response(ADS_TXT, {
+        headers: {
+          "content-type": "text/plain; charset=utf-8",
+          "cache-control": "public, max-age=300"
+        }
+      });
+    }
+
     const response = await env.ASSETS.fetch(request);
     if (response.status !== 404) return response;
 
-    const url = new URL(request.url);
     if (!shouldServeAppShell(url.pathname)) return response;
 
     const indexUrl = new URL(request.url);
